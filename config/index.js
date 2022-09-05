f601e6dfa1f75cc78551bbfc3019a2ec874bb398
2002MQQ <112842420+2002MQQ@users.noreply.github.com>
2022-09-05 13:02:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,10 +8,58 @@ export const config = {
 
   // 公众号APP_SECRET
   APP_SECRET: "7f0659a272ca6ca9b590d969548807fb",
-  TEMPLATE_ID:"PnT6W5LpaEijWHUnAOPKQO1TkMZDFxb3h6OSAnxTmH0",
-  USERS: ["o-dME5zgD2AlD7QDT3fykX--0VX4","o-dME5y9Hfr-zxc6aIzfQbJLl0KI"],
-  
-  
+ /**
+   * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
+   * [
+   *  {
+   *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+   *    id: "o-dME5zgD2AlD7QDT3fykX--0VX4", "o-dME5y9Hfr-zxc6aIzfQbJLl0KI","o-dME5xIR87fz_H53y9YiD5EApfc",
+   *    // 你想对他发送的模板消息的模板ID
+   *    useTemplateId: "PnT6W5LpaEijWHUnAOPKQO1TkMZDFxb3h6OSAnxTmH0",
+   *   },
+   * ]
+   */
+  USERS: [
+    {
+      // 想要发送的人的名字
+      name: "老婆0",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      // 所在省份
+      province: "陕西",
+      // 所在城市
+      city: “西安",
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "老婆1", 
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "老婆2",
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "老婆3", 
+      id: "",
+      useTemplateId: "",
+      province: "",
+      city: "",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+  ],
+
   /**
    * 信息配置
    */
